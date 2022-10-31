2a66e25f25f2fa135d81da5be50ac7b69650262c
Samrat Majumder <majumdersamrat26@gamil.com>
2022-11-01 01:17:27 +0530

    aster

---
+++ src/scenes/golbal/SideBar.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { ProSidebarProvider, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+// import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
+const SideBar = () => {
+  return (
+    <div>
+      <h1>SideBar</h1>
+    </div>
+  );
+};
+
+export default SideBar;
