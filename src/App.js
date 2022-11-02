e87fbc49ab765a0f355cccabd57e20f66d5890e7
Samrat Majumder <majumdersamrat26@gamil.com>
2022-11-02 11:26:04 +0530

    aster

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/golbal/Topbar";
 import SideBar from "./scenes/golbal/SideBar";
 import Dashboard from "./scenes/dashboard/Dashboard";
-// import Team from './scenes/dashboard/Team';
-// import Contacts from './scenes/dashboard/Contacts';
-// import Invoices from './scenes/dashboard/Invoices';
+import Team from "./scenes/team/Team";
+import Contacts from "./scenes/contacts/Contacts";
+import Invoices from "./scenes/invoices/Invoices";
 // import Bar from './scenes/dashboard/Bar';
-// import Form from './scenes/dashboard/Form';
+import Form from "./scenes/form/Form";
 // import Line from './scenes/dashboard/Line';
 // import Pie from './scenes/dashboard/Pie';
 // import FAQ from './scenes/dashboard/Faq';
@@ -27,10 +27,10 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team/>}/> */}
-              {/* <Route path="/contacts" element={<Contacts/>}/> */}
-              {/* <Route path="/invoices" element={<Invoices/>}/> */}
-              {/* <Route path="/form" element={<Form/>}/> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar/>}/> */}
               {/* <Route path="/pie" element={<Pie/>}/> */}
               {/* <Route path="/line" element={<Line/>}/> */}
--- src/scenes/dashboard/Dashboard.js
+++ src/scenes/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
   );
 };
 
