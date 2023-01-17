6820c77a0860e0ec97cfba7c67683a0d94f1a8f2
Samrat Majumder <majumdersamrat26@gamil.com>
2023-01-17 09:53:06 +0530

    aster

--- src/scenes/golbal/SideBar.js
+++ src/scenes/golbal/SideBar.js
@@ -105,7 +105,7 @@ const SideBar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0 " }}
                 >
-                  Ed Roh
+                  Mark Zuckerberg
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
